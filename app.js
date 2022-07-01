091937adb53a97dfe9885719258af15b909c9665
ThatUche <newuser@Uches-MacBook-Air.local>
2022-06-30 20:21:03 -0400

    front end setup with Angularjs

---
+++ app.js
@@ -0,0 +1,16 @@
+(function () {
+
+'use strict'; 
+
+const app = angular.module('MyFoodApp', []);
+
+app.controller('MyFirstcontroller', function($scope, $http){
+   $http.get("https://ancient-beach-68054.herokuapp.com/api/foods").then(response => {
+    $scope.allData = response.data
+        console.log($scope.allData)
+    })
+// console.log($scope.data)
+
+});
+
+})();
