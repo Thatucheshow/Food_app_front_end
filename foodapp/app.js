37493510dd011ba6a960ebfc222461c53c58b5f3
ThatUche <newuser@Uches-MacBook-Air.local>
2022-07-01 13:25:37 -0400

    able to render api data on browser

--- foodapp/app.js
+++ foodapp/app.js
@@ -1,16 +1,29 @@
-// (function () {
+(function () {
 
-// 'use strict'; 
+'use strict'; 
 
 const app = angular.module('MyFoodApp', []);
 
-app.controller('MyFirstcontroller', function($scope, $http){
-   $http.get("https://ancient-beach-68054.herokuapp.com/api/foods").then(response => {
-    $scope.allData = response.data
+app.controller('MyFirstcontroller', function($scope, $http) {
+    // $scope.allData = "allData"
+    $scope.baseUrl = "https://ancient-beach-68054.herokuapp.com/api/foods"
+    $http({
+        method: "GET",
+        url: $scope.baseUrl
+    }).then(response => {
+        $scope.allData = response.data;
         console.log($scope.allData)
-    })
-// console.log($scope.data)
+    }, error => {
+        console.error(error);
+    }).catch(err => console.error('Catch: ' , err));
+    
 
 });
 
-// })();
+})();
+
+// $http){
+//     $http.get("https://ancient-beach-68054.herokuapp.com/api/foods").then(response => {
+//      $scope.allData = response.data
+//          console.log($scope.allData)
+//      })
