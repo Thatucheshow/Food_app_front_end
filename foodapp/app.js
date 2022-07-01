0d32f1a700baf5cd66883225f2e78b04b0364acb
ThatUche <newuser@Uches-MacBook-Air.local>
2022-07-01 16:54:55 -0400

    setting the post route

--- foodapp/app.js
+++ foodapp/app.js
@@ -5,18 +5,37 @@
 const app = angular.module('MyFoodApp', []);
 
 app.controller('MyFirstcontroller', function($scope, $http) {
-    // $scope.allData = "allData"
-    $scope.baseUrl = "https://ancient-beach-68054.herokuapp.com/api/foods"
-    $http({
-        method: "GET",
-        url: $scope.baseUrl
-    }).then(response => {
-        $scope.allData = response.data;
-        console.log($scope.allData)
-    }, error => {
+    $scope.allData = "";
+    $scope.baseUrl = "https://ancient-beach-68054.herokuapp.com/api/foods";
+        $http({
+            method: "GET",
+            url: $scope.baseUrl
+        }).then(response => {
+            $scope.allData = response.data;
+            console.log($scope.allData)
+        }, error => {
         console.error(error);
-    }).catch(err => console.error('Catch: ' , err));
-    
+        }).catch(err => console.error('Catch: ' , err));
+        $scope.postData = "";
+        $scope.postUrl = "https://ancient-beach-68054.herokuapp.com/api/foods"
+             $http({
+                 method: "POST",
+                 url: $scope.postUrl
+             }).then(response => {
+                 $scope.postData = response.data;
+             })
+
+});
+
+app.controller('MyPostcontroller', function($scope, $http) {
+        $scope.postData = "";
+        $scope.postUrl = "https://ancient-beach-68054.herokuapp.com/api/foods"
+             $http({
+                 method: "POST",
+                 url: $scope.postUrl
+             }).then(response => {
+                 $scope.postData = response.data;
+             })
 
 });
 
